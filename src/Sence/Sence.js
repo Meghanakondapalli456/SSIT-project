b21fd944dacaad64adc826dbf13a39be435b3345
Meghana <meghanakondapalli2002@gmail.com>
2025-01-03 16:54:12 +0530

    sense

--- src/Sence/Sence.js
+++ src/Sence/Sence.js
@@ -4,15 +4,26 @@ import Webinar from '../Webinars/Webinar'
 import News from '../News/News'
 import Keyword from '../Keyword/Keyword'
 import { AboutUs } from "../Components/AboutUs/AboutUs";
+import styles from './Sence.module.css'
+import Head from "../Header/Head";
 
 const Sence = () => {
   return (
-    <div>
-        <AboutUs />
+    <div className={styles.wrapper}>
+      <Head className={styles.Head} />
+      <div className={styles.card}>
+      <div className={styles.image}>
+        <img  src='/ssit img.png' className={styles.ssitimg}/>
+        <p className={styles.edu}>We Set, Educate, Nexus, Solve, and Emerge <span className={styles.sense}>SENSE </span>
+        the technology.</p>
+        <button className={styles.btn}>Get in Touch</button>
+      </div>
+        <AboutUs  className={styles.aboutus}/>
         < Whatwe />
         < Webinar />
         < News />
         <Keyword />
+        </div>
     </div>
   )
 }
